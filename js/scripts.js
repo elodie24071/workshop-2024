74dc720b1ddb569f5e80a03ee3e26fdcf7ba498c
Elodie <elodie.quach.24071@stu.he-ferrer.eu>
2024-11-05 15:46:10 +0100

    page d'accueil

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
     console.log('ready');
-    
-});// ready
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Configurer ScrollTrigger pour la partie texte
+    ScrollTrigger.create({
+        trigger: '.etage-texte',
+        pin: true,
+        start: 'top top', // Épingler quand le haut de .etage-texte atteint le haut de la fenêtre
+        endTrigger: '.contenu-etage',
+        end: 'top bottom', // Arrête l'épinglage lorsque le bas de .contenu-etage atteint le bas de la fenêtre
+        markers: true // Affiche les marqueurs pour le débogage
+    });
+});
