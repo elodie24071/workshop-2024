bd0e0e34bdc5e9a05ec6c9e4966a3d73c490f3bb
Elodie <elodie.quach.24071@stu.he-ferrer.eu>
2024-11-06 21:20:49 +0100

    test 3D

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,6 @@ $(document).ready(function () {
 
     gsap.registerPlugin(ScrollTrigger);
 
-    // Configurer ScrollTrigger pour la partie texte
-    // ScrollTrigger.create({
-    //     trigger: '.etage-texte',
-    //     pin: true,
-    //     start: 'top top', // Épingler quand le haut de .etage-texte atteint le haut de la fenêtre
-    //     endTrigger: '.contenu-etage',
-    //     end: 'top bottom', // Arrête l'épinglage lorsque le bas de .contenu-etage atteint le bas de la fenêtre
-    //     markers: true // Affiche les marqueurs pour le débogage
-    // });
-
     gsap.to('#logo-text',  {
         scrollTrigger: {
             trigger: ".hero", 
@@ -20,7 +10,7 @@ $(document).ready(function () {
             end: "bottom top", 
             scrub: true,
         },
-        scale: 0.5, // Réduit la taille à 50%
+        scale: 0.2, // Réduit la taille à 50%
         transformOrigin: "center center", // Centre de réduction
     });
 });
