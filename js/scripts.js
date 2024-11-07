ee6797e7868237b0c2676f292b660e4317703218
Elodie <elodie.quach.24071@stu.he-ferrer.eu>
2024-11-07 09:12:43 +0100

    test isotope

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,52 @@ $(document).ready(function () {
         transformOrigin: "center center", // Centre de réduction
     });
 });
+
+var url = 'https://script.googleusercontent.com/macros/echo?user_content_key=NCTiZGAALg3H4cOh7M4V92jcv-q_UXP-eaMEHXT8KiO-teH6tiVzMynXCWX8ZlqDa0C11z10avhs76-w2-YIUcEL3jp9X-uem5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnK_Fc1f1kAIoNxghPtuRUWyeShsy5YcMsFG2dNNuyo4Lkr63vIJDqWHkuKTg2R_yOAXv9xyx_0Ewoh8GUjLUB0IlEPHcNgCultz9Jw9Md8uu&lib=M1t8Iy23AIs3gZKwR8-bcKYtUlWpmDh-k';
+
+$.ajax({
+    type: 'GET',
+    url: url,
+    dataType: 'json',
+    success: function(data) {
+        console.log(data);
+        
+        var moncontenu = '';
+        $.each(data, function(index, post) {
+            moncontenu += `
+                    <div class="" style="border: 2px solid;">
+                        ${titre} <br>
+                        ${description}
+                    </div>`;
+        });
+        
+        // Ajouter le contenu généré dans le DOM
+        $('.row').html(moncontenu);
+        console.log("Contenu HTML généré :", moncontenu);
+
+        // Initialiser Isotope
+        var iso = new Isotope('.row', {
+            itemSelector: '.item',
+            layoutMode: 'fitRows'
+        });
+
+         // Bouton btn1 pour afficher uniquement info étage 1
+         $('.btn1').on('click', function () {
+            ${}
+        });
+
+         // Bouton btn2 pour afficher uniquement info étage 1
+         $('.btn2').on('click', function () {
+            ${}
+        });
+         // Bouton btn3 pour afficher uniquement info étage 1
+         $('.btn3').on('click', function () {
+            ${}
+        });
+
+    }, // fin success
+    error: function() {
+        alert('An error occurred while loading content.');
+    } // fin error
+}); // fin ajax
+
